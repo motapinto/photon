5508917a626c56b4e317b9101f9430b08b87dca4
Nuno Cardoso <up201706162@fe.up.pt>
2021-04-18 19:14:56 +0100

    Focus on node on click

--- photon/src/view/pages/Graph.tsx
+++ photon/src/view/pages/Graph.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import ForceGraph3D from '3d-force-graph';
+import ForceGraph3D, { ForceGraph3DInstance } from '3d-force-graph';
 import { useGetResource } from "../../viewmodel/hooks/getResource";
 import { getGraphData } from "../../viewmodel/providers/getGraphData";
 
@@ -7,15 +7,33 @@ type GraphData = {
     nodes: [],
     links: [],
 }
+let myGraph: ForceGraph3DInstance;
+
+function focusNode(node: any) {
+    // Aim at node from outside it
+    const distance = 40;
+    const distRatio = 1 + distance/Math.hypot(node.x, node.y, node.z);
+
+    myGraph.cameraPosition(
+        { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }, // new position
+        node, // lookAt ({ x, y, z })
+        2000  // ms transition duration
+    );
+}
 
 export default function Graph(): JSX.Element {
     const resource = useGetResource(getGraphData).data as GraphData;
+    myGraph = ForceGraph3D();
 
     useEffect(() => {
-        const myGraph = ForceGraph3D();
         let element = document.getElementById("graph");
-        if (element != null) 
-            myGraph(element).graphData(resource);
+        if (element) {
+            myGraph(element).graphData(resource)
+                            .onNodeHover(node => {
+                                if (element) element.style.cursor = node ? "pointer" : "auto"
+                            })
+                            .onNodeClick(focusNode);
+        }
     }, [resource]);
 
     return (
