acd9ea5c36f6a20d23442202ca563de2c4f03f38
Martim de Carvalho e Sousa Pinto da Silva <martimpintodasilva@gmail.com>
2021-04-19 10:30:10 +0100

    Add /graph endpoint

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,29 +1,12 @@
 import express, { Request, Response } from 'express';
-import { Record } from 'neo4j-driver';
 import Database from './database/Database';
 
 (async () => {
     const app = express();
-    app.get('/', (_req: Request, res: Response) => {
-        res.send( 'This is the main page!' );
-    });
 
     app.get('/graph', async(_req: Request, res: Response) => {
         const records = await Database.getInstance().getGraph() ?? [];
-        const record1: Record = records[1];
-        console.log(record1.get('origin'));
-        console.log(record1.get('edge'));
-        console.log(record1.get('dest'));
-        
-        res.status(200).send( 'This is the graph visualization page!' );
-    });
-
-    app.get('/about', (_req: Request, res: Response) => {
-        res.send( 'This is the about page' );
-    });
-
-    app.get('/contributors', (_req: Request, res: Response) => {
-        res.send( 'This is the contributors page' );
+        res.status(200).json(records);
     });
 
     const PORT = process.env.SERVER_PORT || 5000;
