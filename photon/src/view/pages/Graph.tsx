2748c4027cc857796cb1bd28db36504c58eca397
Nuno Cardoso <up201706162@fe.up.pt>
2021-04-19 10:01:38 +0100

    NodePopup receives now the node id on click

--- photon/src/view/pages/Graph.tsx
+++ photon/src/view/pages/Graph.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import ForceGraph3D, { ForceGraph3DInstance } from '3d-force-graph';
+import React, { useEffect, useState } from "react";
+import ForceGraph3D from '3d-force-graph';
 import { useGetResource } from "../../viewmodel/hooks/getResource";
 import { getGraphData } from "../../viewmodel/providers/getGraphData";
 import Popup from "../components/NodePopup";
@@ -8,28 +8,26 @@ type GraphData = {
     nodes: [],
     links: [],
 }
-let myGraph: ForceGraph3DInstance;
-let focusedNodeId = 1;
-
-function focusNode(node: any) {
-    // Aim at node from outside it
-    const distance = 70;
-    const distRatio = 1 + distance/Math.hypot(node.x, node.y, node.z);
-
-    myGraph.cameraPosition(
-        { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }, // new position
-        node, // lookAt ({ x, y, z })
-        2000  // ms transition duration
-    );
-
-    focusedNodeId = node.id;
-}
 
 export default function Graph(): JSX.Element {
     const resource = useGetResource(getGraphData).data as GraphData;
-    myGraph = ForceGraph3D();
+    const myGraph = ForceGraph3D();
+    const [focusedNodeId, setFocusedNodeId] = useState(1);
 
     useEffect(() => {
+        function focusNode(node: any) {
+            // Aim at node from outside it
+            const distance = 70;
+            const distRatio = 1 + distance/Math.hypot(node.x, node.y, node.z);
+        
+            myGraph.cameraPosition(
+                { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }, // new position
+                node, // lookAt ({ x, y, z })
+                2000  // ms transition duration
+            );
+        
+            setFocusedNodeId(node.id);
+        }
         let element = document.getElementById("graph");
         if (element) {
             myGraph(element).graphData(resource)
@@ -38,6 +36,7 @@ export default function Graph(): JSX.Element {
                             })
                             .onNodeClick(focusNode);
         }
+        // eslint-disable-next-line
     }, [resource]);
 
     return (
