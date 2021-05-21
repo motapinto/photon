0b5fdade907c8ef3cefe434ce9132b46ad28ac91
Miguel Pinto <pt.miguel99@gmail.com>
2021-05-21 19:44:38 +0100

    Start article extraction and linking

--- backend/src/extract/NewsExtractor.ts
+++ backend/src/extract/NewsExtractor.ts
@@ -1,5 +1,5 @@
 import { errorLogger, infoLogger } from '@logger';
-import { Article } from '@model/Article';
+import { Article, ArticleModel } from '@model/Article';
 import { Record } from 'neo4j-driver';
 import HttpClient from './HttpClient';
 
@@ -31,7 +31,6 @@ export default class NewsExtractor extends HttpClient {
   ];
 
   private constructor() {
-    console.log("OH QUE CARALHO"); 
     super(NewsExtractor.url, {
       'x-rapidapi-key': process.env.NEWS_API_KEY,
       'x-rapidapi-host': 'contextualwebsearch-websearch-v1.p.rapidapi.com',
@@ -68,12 +67,33 @@ export default class NewsExtractor extends HttpClient {
   }
 
   public async processNodes(labels: string[]) {
-    console.log(labels);
-    
-    // TODO: send request for each ontology node record
+    return Promise.all(labels.map(async label => {   
+      try {
+        console.log(label);
+
+        const news = await super.get<NewsApiResponse>({
+          params: {
+            q: label,
+            pageNumber: '1',
+            pageSize: '10', // TODO: change back to 50
+            autoCorrect: 'true',
+            fromPublishedDate: 'null',
+            toPublishedDate: 'null',
+          }
+        });
+
+        await Promise.all(news.value.map(async (article) => await this.processArticle(label, article)));
+
+      } catch (err) {
+        errorLogger.error(err.message);
+      }         
+    })); 
   }
 
-  private async processArticle(article: Article) {
-    infoLogger.info(article);
+  private async processArticle(energyLabel: string, article: Article) {
+    console.log(article);
+    const articleModel = new ArticleModel(article);
+    await articleModel.add();
+    await articleModel.linkToEnergy(energyLabel);
   }
 }
--- backend/src/model/Article.ts
+++ backend/src/model/Article.ts
@@ -1,7 +1,9 @@
+import Database from "@database/Database";
 import { Node } from "./Node";
 
 export interface Article extends Node {
   readonly properties: {
+    id: string,
     title: string,
     url: string,
     description: string,
@@ -9,4 +11,40 @@ export interface Article extends Node {
     datePublished: string,
     score: number,
   };
+}
+
+interface ArticleProperties {
+  id: string,
+  title: string,
+  url: string,
+  description: string,
+  body: string,
+  datePublished: string,
+  score: number,
+}
+
+export class ArticleModel {
+  private db: Database = Database.getInstance();
+  private articleProperties: ArticleProperties;
+  private static articleLabel = "Article";
+
+  public constructor(article: Article) {
+    this.articleProperties = article.properties;
+  }
+
+  public getData(): Node {
+    return { label: ArticleModel.articleLabel, properties: this.articleProperties };
+  }
+
+  public add(): Promise<any> {
+    return this.db.createOrGetNode(this.getData());
+  }
+
+  public linkToEnergy(energyLabel: string) {
+    return this.db.query(`
+      MATCH (origin:Resource {rdfs__label: "${energyLabel}"}), (dest:Tweet {id: "${this.articleProperties.id}"})
+      MERGE (origin)-[e:HasArticle]->(dest)
+      RETURN origin, e, dest
+    `);
+  }
 }
